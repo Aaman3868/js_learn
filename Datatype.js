fc50a1a311b1f7d65504d5d498a2b6b1a5de81d6
Aaman3868 <77456667+Aaman3868@users.noreply.github.com>
2025-06-09 20:53:30 +0000

    datatypes

---
+++ Datatype.js
@@ -0,0 +1,21 @@
+// Data types in js
+"use strict"; // treat all  js code as newer version
+
+let name = "Aaman"; //string
+let age = 26 // integer
+let isloggin = true //boolean
+
+//null  is standalone value
+//undefined  no value assinged
+//symbol  used unique for the componenets 
+
+//object 
+// console.log(typeof undefined);
+
+
+// 2.  conversion
+let score = 11; 
+// console.log(typeof score);
+
+let valueinnumeber = String(score)
+console.log(typeof valueinnumeber);
