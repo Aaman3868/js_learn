e666f67cb14f67ba952a9f7977867117e4115601
Aaman3868 <77456667+Aaman3868@users.noreply.github.com>
2025-06-09 20:30:23 +0000

    let var const

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello Aaman");
+// console.log("Hello Aaman");
+
+// var let const topic
+const accountId = 123456 ;
+let email = "aaman@gmail.com";
+var pass = "12345";
+city = "kolhapur";
+
+// console.log(city);
+console.table([accountId,pass,email]);
