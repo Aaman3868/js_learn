48dba761f4600f75e9e9ec24d0a3f6a3bc970695
Aaman3868 <77456667+Aaman3868@users.noreply.github.com>
2025-06-10 19:17:07 +0000

    learn strings

---
+++ operations.js
@@ -0,0 +1,26 @@
+// Operations js
+
+// let value = 2 
+// let neg = -value
+
+// console.log(neg);
+
+
+console.log("1" + 2);   
+
+// premitive datatye
+// string , Number,Boolean,null undefined symbol , bigInt
+
+// Refernece type 
+// Array , Objects , functons 
+
+
+// dynamic/static 
+
+// Memory space
+
+// stack memory/heap memory
+
+// stack is used in the premetive type 
+// heap is used in the non premitive
+
---
+++ string.js
@@ -0,0 +1,19 @@
+const name = "Aaman"
+const couunt = 45
+
+console.log(`my name is ${name}`);
+console.log(name[0]);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('m'));
+const abc = name.substring(0,3);
+console.log(abc);
+const abcd = name.slice(-4,3);
+console.log(abcd);
+const news = "  Aaman ";
+console.log(news.trim());
+
+
+const url = "https://aaman.com"
+console.log(url.replace('am', 'chinu'));
