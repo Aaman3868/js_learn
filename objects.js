00b9b52578f0fdfd045a6af844113de8c6d3470a
Aaman3868 <77456667+Aaman3868@users.noreply.github.com>
2025-06-12 05:39:41 +0000

    objects

---
+++ objects.js
@@ -0,0 +1,45 @@
+// singleton
+
+// object literals
+
+const users = {
+    name :"aman",
+     age :25,
+     location:"ichalkaranji",
+     email : "aman@gmail.com"
+}
+
+// console.log(users['email']);
+
+const fb = {
+    fullname : {
+        firstname:"Aaman",
+        lastname :"M",
+    },
+    email :"aman@gmail.com"
+}
+
+console.log(fb);
+
+
+
+
+// de structure
+
+const course = {
+    coursename :"jx",
+    price :"900",
+    developer : "aaman"
+}
+
+const  {developer} = developer
+
+console.log(developer);
+
+{
+    "coursename" :"jx",
+    "price" :"900",
+    "developer" : "aaman"
+}
+
+
